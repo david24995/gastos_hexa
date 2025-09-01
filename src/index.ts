6041d5d7bb70b138711cd27a1fe9b22724a487e3
David Huamaccto Chate <davidhuamaccto24995@gmail.com>
2025-09-01 17:12:48 -0500

    Agregando prisma y configurando un poco las variables de entorno

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
+// import { PrismaClient } from './generated/prisma';
+import { PrismaClient } from '@prisma/client';
 import { UsuarioUseCase } from './usuario/application/usuario.useCase';
 import { UsuarioOperation } from './usuario/infrastructure/usuario.operation';
+
+const prisma = new PrismaClient();
 
 (async () => {
   await main();
@@ -9,5 +13,16 @@ async function main() {
   const usuarioOperation = new UsuarioOperation();
   const userUseCase = new UsuarioUseCase(usuarioOperation);
 
-  console.log(await userUseCase.list());
+  // console.log(await userUseCase.list());
+
+  const newUser = await prisma.user.create({
+    data: {
+      name: 'David2',
+      email: 'david2@gmail.com',
+      fecha_nacimiento: new Date('1995/09/24'),
+      password: '123',
+    },
+  });
+
+  console.log(newUser);
 }
