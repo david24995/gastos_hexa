74803e2c397c08ad3b5d7a032a7cd00fa91a7251
David Huamaccto Chate <davidhuamaccto24995@gmail.com>
2025-09-01 18:03:05 -0500

    Probando la config de Prisma - ahora falta el DTO

--- src/usuario/infrastructure/usuario.operation.ts
+++ src/usuario/infrastructure/usuario.operation.ts
@@ -1,19 +1,16 @@
+import { PrismaClient } from '../../generated/prisma';
 import { Estado } from '../../shared/domain/estado.model';
 import { UsuarioRepository } from '../application/usuario.repository';
 import { UsuarioModel } from '../domain/usuario.model';
 
+const prisma = new PrismaClient();
+
 export class UsuarioOperation implements UsuarioRepository {
-  list(): Promise<UsuarioModel[]> {
-    return Promise.resolve([
-      {
-        id: 1,
-        nombre: 'David',
-        apellido: 'Huamaccto',
-        email: 'davidhuamaccto24995@gmail.com',
-        fecha_nacimiento: new Date('2025-09-24'),
-        estado: Estado.ACTIVO,
-      },
-    ]);
+  async list(): Promise<UsuarioModel[]> {
+    const users = await prisma.user.findMany();
+
+    // return users.map((usr) => ({ id: usr.id,  }))
+    return [];
   }
   getOne(id: number): Promise<UsuarioModel> {
     return Promise.resolve({
